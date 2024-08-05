16030d0fb8d2fd575a7d48af8d6119198348f2a7
agonzalez <agonzalez@planfor.fr>
2024-08-05 16:41:02 +0200

    lax

--- frontendapp/src/axios.js
+++ frontendapp/src/axios.js
@@ -1,12 +1,17 @@
 import { API_URL } from './config';
+import Cookies from 'js-cookie'; // Importez js-cookie
 
 async function fetchWithCredentials(endpoint, method = 'GET', body = null) {
     const url = `${API_URL}${endpoint}`;
+
+    // Récupérez le token CSRF du cookie
+    const csrfToken = Cookies.get('XSRF-TOKEN'); // Assurez-vous que le nom du cookie correspond à celui utilisé par votre application
 
     const options = {
         method: method,
         headers: {
             'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken, // Ajoutez le token CSRF aux en-têtes
         },
         credentials: 'include',
     };
