35965a1c5a98c5b95fb841875bbb2b5f22aa793a
agonzalez <agonzalez@planfor.fr>
2024-08-19 15:41:18 +0200

    + duration session

--- config/session.php
+++ config/session.php
@@ -6,7 +6,7 @@ return [
 
     'driver' => env('SESSION_DRIVER', 'file'),
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 720),
 
     'expire_on_close' => false,
 
--- frontendapp/src/main.js
+++ frontendapp/src/main.js
@@ -1,20 +1,37 @@
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store';
-import { AdMob} from '@capacitor-community/admob';
+import { AdMob } from '@capacitor-community/admob';
 
 import '@/styles/app.scss'
-
-AdMob.initialize({
-    requestTrackingAuthorization: true,
-    testingDevices: ['ca-app-pub-3940256099942544/6300978111'],
-    initializeForTesting: false,
-  });
 
 const app = createApp(App)
 app.use(router)
 app.use(store);
 
+AdMob.initialize({
+    requestTrackingAuthorization: true,
+  })
+    .then(() => {
+      AdMob.requestConsentInfo({
+        // ...
+      })
+        .then(result => {
+          console.log('Réponse de AdMob.requestConsentInfo:', result);
+          if (result.trackingAuthorizationStatus === 'AUTHORIZED') {
+            // L'utilisateur a autorisé le suivi
+            // ...
+          } else {
+            // L'utilisateur n'a pas autorisé le suivi
+            // ...
+          }
+        })
+        .catch(error => {
+          console.error('Erreur lors de la demande de consentement:', error);
+        });
+    })
+    .catch(error => {
+      console.error("Erreur lors de l'initialisation d'AdMob:", error);
+    });
 app.mount('#app')
