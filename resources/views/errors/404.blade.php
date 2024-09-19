0600e3c6deecaf9c15e88ed25c6aa802dd77a9d5
agonzalez <agonzalez@planfor.fr>
2024-09-19 12:58:22 +0200

    +error404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div id="404-page" style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 80vh; width: 100%; ">
+    <h1>404</h1>
+    <p>Page introuvable</p>
+</div>
+
+
+@endsection
